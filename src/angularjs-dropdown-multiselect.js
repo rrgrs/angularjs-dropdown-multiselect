4f065581c8a7c39031c9cca8029c30c68edcb97c
Ryan Rogers <ryan.rogers@ckrinteractive.com>
2016-11-01 16:33:32 -0700

    parent button now a p tag

--- src/angularjs-dropdown-multiselect.js
+++ src/angularjs-dropdown-multiselect.js
@@ -20,7 +20,7 @@ directiveModule.directive('ngDropdownMultiselect', ['$filter', '$document', '$co
                 var groups = attrs.groupBy ? true : false;
 
                 var template = '<div class="SumoSelect multiselect-parent" ng-class="{open: open}">';
-                template += '<button class="CaptionCont SelectBox search" title="{{getButtonText()}}" ng-click="toggleDropdown()"><span>{{getButtonText()}}</span><label><i></i></label></button>';
+                template += '<p class="CaptionCont SelectBox search" title="{{getButtonText()}}" ng-click="toggleDropdown()"><span>{{getButtonText()}}</span><label><i></i></label></p>';
                 template += '<div ng-class="{isFloating: browser_is_small}" ng-style="{display: open ? \'block\' : \'none\'}" class="optWrapper multiple"><ul class="options">';
                 template += '<li ng-hide="!settings.showCheckAll || settings.selectionLimit > 0"><a data-ng-click="selectAll()"><span class="glyphicon glyphicon-ok"></span>  {{texts.checkAll}}</a>';
                 template += '<li ng-show="settings.showUncheckAll"><a data-ng-click="deselectAll();"><span class="glyphicon glyphicon-remove"></span>   {{texts.uncheckAll}}</a></li>';
